59f015d3ead26fc245b9b0726bc9cbedd21baa8a
BungTH <teerasan.personal@gmail.com>
2025-01-25 15:53:36 +0700

    Update: add credentail for bigquery WITHOUT keyfile

--- example/from-local-to-bigquery/example_upload_local_file_to_bigquery.py
+++ example/from-local-to-bigquery/example_upload_local_file_to_bigquery.py
@@ -1,13 +1,33 @@
+import json
 from google.cloud import bigquery
-import os
+from google.oauth2 import service_account
 
-def load_to_bigquery(local_file_path, project_id, dataset_id, table_id):
-    client = bigquery.Client(project=project_id)
+def load_to_bigquery():
+    local_file_path = "greenery_data/addresses.csv"
+    project_id = "basic-carrier-448907-q3"
+    dataset_id = "greenery"
+    table_id = "addresses"
+    keyfile = "keyfile/gcs-credentials.json"
+    
+    service_account_info = json.load(open(keyfile))
+    credentials = service_account.Credentials.from_service_account_info(service_account_info)
+    
+    client = bigquery.Client(
+        project=project_id,
+        credentials=credentials,
+    )
 
     table_ref = client.dataset(dataset_id).table(table_id)
     job_config = bigquery.LoadJobConfig(
         source_format=bigquery.SourceFormat.CSV,
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        schema=[
+            bigquery.SchemaField("address_id", "STRING"),
+            bigquery.SchemaField("address", "STRING"),
+            bigquery.SchemaField("zipcode", "STRING"),
+            bigquery.SchemaField("state", "STRING"),
+            bigquery.SchemaField("country", "STRING"),
+        ]
     )
 
     with open(local_file_path, "rb") as source_file:
@@ -17,10 +37,10 @@ def load_to_bigquery(local_file_path, project_id, dataset_id, table_id):
 
     print(f"Loaded {job.output_rows} rows into {dataset_id}:{table_id}.")
 
-if __name__ == "__main__":
-    local_file_path = '/path/to/local/file.csv'
-    project_id = 'your_project'
-    dataset_id = 'your_dataset'
-    table_id = 'your_table'
+# if __name__ == "__main__":
+#     local_file_path = '/path/to/local/file.csv'
+#     project_id = 'your_project'
+#     dataset_id = 'your_dataset'
+#     table_id = 'your_table'
 
-    load_to_bigquery(local_file_path, project_id, dataset_id, table_id)
+load_to_bigquery()
